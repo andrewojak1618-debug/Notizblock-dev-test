1a293acb69746112f9c4a9bac22c67d113765065
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-05-22 11:05:44 +0200

    first added html css js

---
+++ script.js
@@ -0,0 +1,18 @@
+// notizen anzeigen lassen
+// ich brauche Notizen
+let notizen = ["banana, apple, orange"];
+
+function renderNotizen() {
+  let contentRef = document.getElementById("content");
+
+  contentRef.innerHTML = notes;
+}
+
+// ich muss definieren, wie eine Notiz aussieht (Titel, Inhalt, Erstellungsdatum, evtl. Tags)
+// wann werden sie angezeigt? (z.B. alle Notizen, nur die aktuellen, etc.)
+
+// global speichern
+// notizen hinzufügen
+
+// notizen löschen
+// notizen archivieren
