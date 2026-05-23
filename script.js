dd0f86d4f96ceb5e7acba4d93fbde565547f0006
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-05-23 11:22:02 +0200

    added index.html and style.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // notizen anzeigen lassen
 // ich brauche Notizen
-let notes = ["1 Notiz", "2 Notiz", "3 Notiz"];
+let notes = ['1 Notiz', '2 Notiz', '3 Notiz'];
 
 function renderNotes() {
-  let contentRef = document.getElementById("content");
-  contentRef.innerHTML = "";
+  let contentRef = document.getElementById('content');
+  contentRef.innerHTML = '';
 
   for (let indexNote = 0; indexNote < notes.length; indexNote++) {
     const note = notes[indexNote];
@@ -16,11 +16,22 @@ function getNoteTemplate(note) {
   return `<p>+ ${note}</p>`;
 }
 
+// notizen hinzufügen
+function addNote() {
+  let noteInputRef = document.getElementById('note_input');
+  let noteInput = noteInputRef.value;
+
+  notes.push(noteInput);
+
+  renderNotes();
+
+  noteInputRef.value = '';
+}
+
 // ich muss definieren, wie eine Notiz aussieht (Titel, Inhalt, Erstellungsdatum, evtl. Tags)
 // wann werden sie angezeigt? (z.B. alle Notizen, nur die aktuellen, etc.)
 
 // global speichern
-// notizen hinzufügen
 
 // notizen löschen
 // notizen archivieren
