5780b6fbfdff053a5a57a1fda3954070fb2ea3fe
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-05-24 14:47:18 +0200

    added script.js

--- script.js
+++ script.js
@@ -19,11 +19,21 @@ function saveData() {
   inputRef.value = '';
 }
 
-function saveToLocalStorage(indexNote) {
+function saveToLocalStorage() {
   localStorage.setItem('myData', JSON.stringify(myData));
 }
 
-function getFromLocalStorage(indexTrashNote) {}
+function getFromLocalStorage() {
+  let myArray = JSON.parse(localStorage.getItem('myData'));
+
+  if (myArray != null) {
+    myData = myArray;
+  }
+}
+//myData soll nicht überschrieben werden. Sonst wäre das Array weg und render() könnte Probleme bekommen.
+//Then myData should not be overwritten. Otherwise, your array would be gone and render() might have problems.
+//Nur wenn wirklich gespeicherte Daten vorhanden sind, überschreibe myData
+//Only if there is really stored data, overwrite myData
 
 function render() {
   let contentRef = document.getElementById('content');
